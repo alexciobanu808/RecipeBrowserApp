bdf72fb0c9e4792b5e74c6916c34fbf640ee3b34
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-04-30 15:53:14 -0400

    Implement DessertMealsViewModel

---
+++ RecipeBrowserApp/ViewModels/DessertMealsViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  DessertMealsViewModel.swift
+//  RecipeBrowserApp
+//
+//  Created by Alex Ciobanu on 4/29/22.
+//
+
+import Foundation
+
+class DessertMealsViewModel {
+    @Published var uiModel: Result<[String], TheMealAPIError>?
+    @Published var isLoading = false
+    
+    private let api = TheMealAPI()
+    
+    var desertNames: [String] {
+        switch uiModel {
+        case .success(let names): return names
+        case .failure, .none: return []
+        }
+    }
+    
+    func fetchDessetMeals() {
+        api.getDessertMeals { [weak self] response in
+            self?.uiModel = response.map { $0.meals.map(\.name).sorted() }
+        }
+        
+    }
+}
