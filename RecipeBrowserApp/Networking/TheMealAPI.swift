c6a25a1b5bf08d8b6219b73b79ecf1cefb79aa34
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-04-30 15:48:30 -0400

    Implement class fetch data from API

---
+++ RecipeBrowserApp/Networking/TheMealAPI.swift
@@ -0,0 +1,45 @@
+//
+//  TheMealAPI.swift
+//  RecipeBrowserApp
+//
+//  Created by Alex Ciobanu on 4/29/22.
+//
+
+import Combine
+import Foundation
+
+class TheMealAPI {
+    private let baseURL = "https://www.themealdb.com/api/json/v1/1/"
+    
+    func getDessertMeals(completionHandler: @escaping (Result<DessertMeals, TheMealAPIError>) -> Void) {
+        let urlString = baseURL + "filter.php?c=Dessert"
+        
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(.invalidURL))
+            return
+        }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failure(.network(error)))
+            } else if let data = data {
+                do {    
+                    let decodedResponse = try JSONDecoder().decode(DessertMeals.self, from: data)
+                    completionHandler(.success(decodedResponse))
+                } catch {
+                    completionHandler(.failure(.decoding))
+                }
+            } else {
+                completionHandler(.failure(.unknown))
+            }
+        }.resume()
+    }
+}
+
+enum TheMealAPIError: Error {
+    case invalidURL
+    case decoding
+    case network(Error)
+    case unknown
+}
