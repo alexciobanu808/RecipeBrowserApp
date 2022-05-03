9865fa2bbaddd8aab1fd1bdc73206fa7449d003f
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-05-02 20:46:53 -0400

    Refactor mealDetails view model

--- RecipeBrowserApp/ViewModels/MealDetailsViewModel.swift
+++ RecipeBrowserApp/ViewModels/MealDetailsViewModel.swift
@@ -8,14 +8,37 @@
 import Foundation
 
 class MealDetailsViewModel {
-    @Published var uiModel: Result<MealDetails, TheMealAPIError>?
-
+    @Published var uiModel: Result<MealDetailsResponse, TheMealAPIError>?
+    
+    private let mealID: String
+    let mealPictureURL: String
+    
     private let api = TheMealAPI()
+    
+    var mealDetails: MealDetailsResponse.MealDetails? {
+        switch uiModel {
+        case .success(let mealDetailsResponse): return mealDetailsResponse.meals.first
+        case.failure, .none: return nil
+        }
+    }
+    
+    var ingredientsAndMeasurementsString: String {
+        guard let details = mealDetails else { return "" }
+        
+        return details.ingredientsAndMeasurements.map { item in
+            "\(item.ingredient) - \(item.measurement)\n"
+        }.joined()
+    }
+    
+    init(mealID: String, mealPictureURL: String) {
+        self.mealID = mealID
+        self.mealPictureURL = mealPictureURL
+    }
     
     
     
     func getMealDetails() {
-        api.getMealDetails(mealId: "") { [weak self] response in
+        api.getMealDetails(mealId: mealID) { [weak self] response in
             self?.uiModel = response
         }
     }
