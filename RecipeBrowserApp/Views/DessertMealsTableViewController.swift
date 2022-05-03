8da2dede8301ee325122a23f33a51056612084b5
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-05-02 20:47:35 -0400

    Refactor dessertmeals vc

--- RecipeBrowserApp/Views/DessertMealsTableViewController.swift
+++ RecipeBrowserApp/Views/DessertMealsTableViewController.swift
@@ -23,15 +23,24 @@ class DessertMealsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        dessertViewModel.desertNames.count
+        dessertViewModel.meals.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MealCellID", for: indexPath)
-        cell.textLabel?.text = dessertViewModel.desertNames[indexPath.row]
+        cell.textLabel?.text = dessertViewModel.meals[indexPath.row].strMeal
         
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: "MealDetailsViewControllerID") as? MealDetailsViewController else { return }
+            
+        let meal = dessertViewModel.meals[indexPath.row]
+        let viewModel = MealDetailsViewModel(mealID: meal.idMeal, mealPictureURL: meal.strMealThumb)
+        viewController.mealDetailsViewModel = viewModel
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
 }
 
